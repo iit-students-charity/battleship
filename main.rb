867b1f26e50c27b0f061f715211a9c293a2c8e14
Alexander <artoriousso@gmail.com>
2018-09-08 15:10:28 +0300

    Define Board and Ship methods

--- main.rb
+++ main.rb
@@ -1,11 +1,34 @@
 class Cell
-  attr_accessor :type
-  attr_reader :x, :y
+  attr_reader :x, :y, :type
 
   def initialize(x, y, type = :empty)
     @x = x
     @y = y
     @type = type
+  end
+
+  def adjoin!
+    @type = :adjoined
+  end
+
+  def empty!
+    @type = :empty
+  end
+
+  def ship!
+    @type = :ship
+  end
+
+  def fired!
+    @type = :fired
+  end
+
+  def fired_ship!
+    @type = :fired_ship
+  end
+
+  def adjoined?
+    @type == :adjoined
   end
 
   def empty?
@@ -26,11 +49,31 @@ class Cell
 end
 
 class Ship
-  def initialize()
+  attr_reader :x, :y, :direction, :length
+
+  def initialize(x, y, direction, length)
+    @x = x
+    @y = y
+    @direction = direction
+    @length = length
+  end
+
+  def coordinates
+    if direction == :up
+      coordinates << length.times do |number|
+        [x, y - number]
+      end
+    end
+    if direction == :right
+      coordinates << length.times do |number|
+        [x + number, y]
+      end
+    end
+  end
 end
 
 class Board
-  attr_reader :size_x, :size_y
+  attr_reader :size_x, :size_y, :ships
 
   def initialize(size_x = 10, size_y = 10)
     @size_x = size_x
@@ -38,11 +81,16 @@ class Board
     set_board
   end
 
-  def set_ships(ships)
-
+  def set_ship(ship)
+    raise IncorrectPlace unless check_ship(ship)
+    #@ships << ship
+    ship.coordinates.each do |pair|
+      @cells.select { |cell| cell.x == pair.first && cell.y == pair.last }.first.ship!
+    end
+    set_adjoined(ship.coordinates)
   end
 
-  def set_random_ships()
+  def set_random_ships
 
   end
 
@@ -60,9 +108,38 @@ class Board
 
   private
 
-  def set_board
-
+  def check_ship(coordinates)
+    coordinates.each do |pair|
+      return false if @cells.select { |cell| cell.x  == pair.first && cell.y == pair.last }.first.ship?
+      return false if @cells.select { |cell| cell.x  == pair.first && cell.y == pair.last }.first.adjpined?
+    end
+    return false if coordinates.map(&:first).each { |x| x > 10 || x < 1 }
+    return false if coordinates.map(&:second).each { |y| y > 10 || y < 1 }
+    true
   end
 
+  def set_board
+    size_x.times do |x|
+      size_y.times do |y|
+        @cells << Cell.new(x + 1, y + 1)
+      end
+    end
+  end
 
+  def set_adjoined(ship)
+    ship.coordinates.each do |pair|
+      adjoined = adjoined_array(pair)
+      adjoined.map(&:adjoin!)
+    end
+  end
+
+  def adjoined_array(pair)
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        adj << @cells.select { |cell| cell.x  == pair.first + i && cell.y == pair.last + j && !cell.ship? }.first
+      end
+    end
+    adj
+  end
 end
+
