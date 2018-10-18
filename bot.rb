3c6597dc554c8163566e02c4e220b17de7b51492
Alexander <artoriousso@gmail.com>
2018-10-18 19:15:34 +0300

    Inherit Bot from Player, add defeated? method to Player, refactor delegation

--- bot.rb
+++ bot.rb
@@ -1,27 +1,2 @@
-require 'forwardable'
-
-class Bot
-  attr_reader :score, :board
-
-  def initialize(board, name = nil)
-    @score = 0
-    @name = name
-    @board = board
-    @printer = Printer.new(@board)
-  end
-
-  extend Forwardable
-  def_delegator :@board, :shot, :shot_on
-  def_delegator :@board, :set_ship
-  def_delegator :@board, :set_ship_randomly
-  def_delegator :@printer, :print_open, :print
-  def_delegator :@printer, :print_hidden
-
-  def increace_score(value = 100)
-    @score += value
-  end
-
-  def decreace_score(value = 10)
-    @score -= value
-  end
+class Bot < Player
 end
--- player.rb
+++ player.rb
@@ -11,17 +11,23 @@ class Player
   end
 
   extend Forwardable
-  def_delegator :@board, :random_shot, :shot_on
+  def_delegator :@board, :random_shot, :random_shot_on
+  def_delegator :@board, :shot, :shot_on
   def_delegator :@board, :set_ship
   def_delegator :@board, :set_ship_randomly
   def_delegator :@printer, :print_open, :print
   def_delegator :@printer, :print_hidden
 
-  def increace_score(value = 100)
+  def increase_score(value = 100)
     @score += value
   end
 
-  def decreace_score(value = 10)
+  def decrease_score(value = 10)
     @score -= value
+    @score = @score < 0 ? 0 : @score
+  end
+
+  def defeated?
+    !@board.cells.map(&:ship?).include?(true)
   end
 end
